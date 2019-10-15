e0a34e03fb0f2795339c6f2601ad03edb4978a93
GMX9 <41052152+GMX9@users.noreply.github.com>
2019-10-15 15:44:32 +0100

    Javascript ES6 Arrow Functions Update

--- cookie-consent.js
+++ cookie-consent.js
@@ -1,58 +1,60 @@
 /* 
+------------------------------------------
 @Package -> Cookie Consent 
-@Version -> v1.0.0
+------------------------------------------
+@Version -> v1.0.2
+------------------------------------------
+@Author -> GMX
+------------------------------------------
 @Description -> Cookie Consent by the user
+------------------------------------------
 */
-function CookieConsent(message,accept_button,decline_button,privacy_btn,privacy_href){
-    
-    var consent = '<div class="cookie-consent"> <div class="container-consent"> <div class="cookie-consent-text"> '+message+' <p><a  style="color:white; text-decoration:underline;" href="'+privacy_href+'">'+privacy_btn+'</a></p> </div> <div class="cookie-consent-button"> <div class="align-consent-btn"> <button id="decline-consent-cookie-consent" class="consent-btn"> '+decline_button+' </button> <button id="give-consent-cookie-consent" class="consent-btn"> '+accept_button+' </button>  </div> </div> </div> </div>';
-    
-    $("body").append(consent);
-    
-    //Check localStorage on each page visit, if it's null, show your div
-    if (!window.localStorage.getItem("cookie_accepted") && !window.localStorage.getItem("cookie_declined")) {
-        //show the div
-        $(".cookie-consent").show();
-        console.log("Consent not set.");
-        //Set the key
-    }
-    
-    console.log(window.localStorage.getItem("cookie_accepted"));
-    
-    $( "#give-consent-cookie-consent" ).click(function() {
-            $(".cookie-consent").hide();
-            window.localStorage.setItem("cookie_accepted", true);
-            console.log("Consent set.");
-    });
-    
-    $( "#decline-consent-cookie-consent" ).click(function() {
-            $(".cookie-consent").hide();
-            window.localStorage.setItem("cookie_declined", true);
-            console.log("Consent declined.");
-    });
-}   
+class CookieConsent{
 
-// Obtain Cookie Consent Status to set other third party cookies | Uses TRUE or F
-function getConsentStatus(){
-    
-    if (window.localStorage.getItem("cookie_accepted")){
-        
-        var value = "cookie_accepted";
+    constructor(message,accept_button,decline_button,privacy_btn,privacy_href){ this.setConsent(message,accept_button,decline_button,privacy_btn,privacy_href); }
 
-    }else if (window.localStorage.getItem("cookie_declined")){
+    setConsent(message,accept_button,decline_button,privacy_btn,privacy_href){
         
-        var value = "cookie_declined";
+        var consent = '<div class="cookie-consent"> <div class="container-consent"> <div class="cookie-consent-text"> '+message+' <p><a  style="color:white; text-decoration:underline;" href="'+privacy_href+'">'+privacy_btn+'</a></p> </div> <div class="cookie-consent-button"> <div class="align-consent-btn"> <button id="decline-consent-cookie-consent" class="consent-btn"> '+decline_button+' </button> <button id="give-consent-cookie-consent" class="consent-btn"> '+accept_button+' </button>  </div> </div> </div> </div>';
+        
+        $("body").append(consent);
+        
+        //Check localStorage on each page visit, if it's null, show your div
+        if (!window.localStorage.getItem("cookie_accepted") && !window.localStorage.getItem("cookie_declined")) {
+            //show the div
+            $(".cookie-consent").show();
+            console.log("Consent not set.");
+            //Set the key
+        }
+        
+        console.log(window.localStorage.getItem("cookie_accepted"));
+        
+        $( "#give-consent-cookie-consent" ).click(() => {
+                $(".cookie-consent").hide();
+                window.localStorage.setItem("cookie_accepted", true);
+                console.log("Consent set.");
+        });
+        
+        $( "#decline-consent-cookie-consent" ).click(() => {
+                $(".cookie-consent").hide();
+                window.localStorage.setItem("cookie_declined", true);
+                console.log("Consent declined.");
+        });
+    }   
 
-    }else{
+    // Obtain Cookie Consent Status to set other third party cookies | Uses TRUE or F
+    getStatus(){
         
-        var value = "cookie_unset";
-        
+        if (window.localStorage.getItem("cookie_accepted")){
+            
+            return true;
+
+        }else{
+            
+            return false;
+
+        }
+
     }
-    
-    if(value == "cookie_accepted"){
-        return true;
-    }else{
-        return false;
-    }
-    
+
 }
